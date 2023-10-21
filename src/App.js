81f84a3b87e3dbf9f24caf1c263504db0fdb9ca1
wasimakram97091 <wasimakram97091@gmail.com>
2023-10-21 08:45:42 +0530

    changes in css

--- src/App.js
+++ src/App.js
@@ -10,21 +10,29 @@ import LoginPage from "./components/loginPage/LoginPage";
 import SignUpPage from "./components/SingUp/SignUpPage";
 import LoginError from "./components/loginErrors/LoginError";
 import SignUpSuccess from "./components/signUpSuccess/SignUpSuccess";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
   return (
     <>
       <Routes>
         <Route path="/" Component={LoginPage} />
-        <Route exact path="/home" Component={LandingPage} />
-        <Route path="/details/:id" Component={DeatilsLanding} />
-        <Route path="/movies" Component={MovieList} />
-        <Route path="/tvShows" Component={TvList} />
-        <Route path="search" Component={SearchData} />
 
-        <Route path="/signUp" Component={SignUpPage} />
-        <Route path="/error" Component={LoginError} />
-        <Route path="/success" Component={SignUpSuccess} />
+        {isAuthenticated && (
+          <>
+            <Route exact path="/home" Component={LandingPage} />
+            <Route path="/details/:id" Component={DeatilsLanding} />
+            <Route path="/movies" Component={MovieList} />
+            <Route path="/tvShows" Component={TvList} />
+            <Route path="search" Component={SearchData} />
+            <Route path="/signUp" Component={SignUpPage} />
+            <Route path="/error" Component={LoginError} />
+            <Route path="/success" Component={SignUpSuccess} />
+          </>
+        )}
       </Routes>
     </>
   );
